cc358c7dcb57c15c2f562b5cc947348c3ab3a5ec
Jamie Saxon <jsaxon@uchicago.edu>
2016-10-02 20:00:27 -0500

    two more problems

---
+++ q6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 
+
+# The 'solution' variable should hold the
+# solution when the script is done.
+solution = 0
+
+# create a function that returns
+# the next value in the collatz sequence.
+# If you call it recursively, keep track of the depth.
+
+# now loop up to 2 million, 
+# using that function
+
+
---
+++ q7.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+
+# The 'solution' variable should hold the
+# solution when the script is done.
+solution = 0
+
+# Your code goes here.
+# Should be < 10 lines.
+
